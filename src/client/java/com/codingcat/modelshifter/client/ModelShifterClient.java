7d3e3155b44dc5c7e94e8d96cfa8465291cfa6e7
codingcat2468 <lw-app-produktion@gmx.net>
2024-08-24 20:24:05 +0200

    Initial commit

---
+++ src/client/java/com/codingcat/modelshifter/client/ModelShifterClient.java
@@ -0,0 +1,10 @@
+package com.codingcat.modelshifter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModelShifterClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
