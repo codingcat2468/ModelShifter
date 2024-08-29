8bf80f5c2298ea9fbadcbdb008133e64fbe8f656
codingcat2468 <lw-app-produktion@gmx.net>
2024-08-29 20:41:37 +0200

    Add wither model

---
+++ src/client/java/com/codingcat/modelshifter/client/impl/model/WitherPlayerModel.java
@@ -0,0 +1,39 @@
+package com.codingcat.modelshifter.client.impl.model;
+
+import com.codingcat.modelshifter.client.ModelShifterClient;
+import com.codingcat.modelshifter.client.api.model.PlayerModel;
+import com.codingcat.modelshifter.client.api.state.DisabledFeatureRenderers;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
+
+public class WitherPlayerModel implements PlayerModel {
+    @Override
+    public Identifier getModelDataIdentifier() {
+        return new Identifier(ModelShifterClient.MOD_ID, "wither_player");
+    }
+
+    @Override
+    public void modifyHeldItemRendering(MatrixStack matrixStack) {
+
+    }
+
+    @Override
+    public void modifyElytraRendering(MatrixStack matrixStack) {
+
+    }
+
+    @Override
+    public @NotNull DisabledFeatureRenderers getDisabledFeatureRenderers() {
+        return new DisabledFeatureRenderers(
+                true,
+                true,
+                false,
+                true,
+                true,
+                true,
+                false,
+                true
+        );
+    }
+}
