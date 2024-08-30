6ce3c28c80e3a498ddc7ea3cc924b86af537b7ef
codingcat2468 <lw-app-produktion@gmx.net>
2024-08-30 20:46:42 +0200

    Fix incorrect class

--- src/client/java/com/codingcat/modelshifter/client/impl/model/Models.java
+++ src/client/java/com/codingcat/modelshifter/client/impl/model/Models.java
@@ -8,7 +8,7 @@ import net.minecraft.util.Identifier;
 public class Models {
     public static final PlayerModel CAT_PLAYER_MODEL = register("cat", new CatPlayerModel());
     public static final PlayerModel AMONG_US_PLAYER_MODEL = register("among_us", new AmongUsPlayerModel());
-    public static final PlayerModel WITHER_PLAYER_MODEL = register("wither", new AmongUsPlayerModel());
+    public static final PlayerModel WITHER_PLAYER_MODEL = register("wither", new WitherPlayerModel());
 
     private static PlayerModel register(String id, PlayerModel playerModel) {
         return ModelRegistry.register(new Identifier(ModelShifterClient.MOD_ID, id), playerModel);
