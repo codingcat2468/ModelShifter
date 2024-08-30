b17376654fd3693b106fbc554d40c883b11c7c08
codingcat2468 <lw-app-produktion@gmx.net>
2024-08-30 21:13:31 +0200

    Rework initialization process

--- src/client/java/com/codingcat/modelshifter/client/ModelShifterClient.java
+++ src/client/java/com/codingcat/modelshifter/client/ModelShifterClient.java
@@ -1,10 +1,14 @@
 package com.codingcat.modelshifter.client;
 
+import com.codingcat.modelshifter.client.impl.model.Models;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ModelShifterClient implements ClientModInitializer {
+    public static final String MOD_ID = "modelshifter";
+    public static AdditionalRendererState additionalRendererState;
 
     @Override
     public void onInitializeClient() {
+        Models.registerAll();
     }
 }
--- src/client/java/com/codingcat/modelshifter/client/api/registry/ModelRegistry.java
+++ src/client/java/com/codingcat/modelshifter/client/api/registry/ModelRegistry.java
@@ -3,7 +3,6 @@ package com.codingcat.modelshifter.client.api.registry;
 import com.codingcat.modelshifter.client.api.model.PlayerModel;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,11 +11,10 @@ import java.util.Set;
 
 public class ModelRegistry {
     @NotNull
-    private final static HashMap<Identifier, PlayerModel> data;
+    private final static HashMap<Identifier, PlayerModel> data = new HashMap<>();
 
-    @Nullable
-    public static PlayerModel register(@NotNull Identifier id, @NotNull PlayerModel model) {
-        return data.put(id, model);
+    public static void register(@NotNull Identifier id, @NotNull PlayerModel model) {
+        data.put(id, model);
     }
 
     @NotNull
@@ -36,9 +34,5 @@ public class ModelRegistry {
     @NotNull
     public static Set<Map.Entry<Identifier, PlayerModel>> entries() {
         return data.entrySet();
-    }
-
-    static {
-        data = new HashMap<>();
     }
 }
