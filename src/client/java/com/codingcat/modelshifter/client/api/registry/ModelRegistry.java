4c70b6983d2a89569758245b05b5017c280727bc
codingcat2468 <lw-app-produktion@gmx.net>
2024-08-30 20:45:58 +0200

    Map entries of ModelRegistry to an Identifier

--- src/client/java/com/codingcat/modelshifter/client/api/registry/ModelRegistry.java
+++ src/client/java/com/codingcat/modelshifter/client/api/registry/ModelRegistry.java
@@ -1,27 +1,34 @@
 package com.codingcat.modelshifter.client.api.registry;
 
 import com.codingcat.modelshifter.client.api.model.PlayerModel;
+import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class ModelRegistry {
     @NotNull
-    private final static Set<PlayerModel> data;
+    private final static HashMap<Identifier, PlayerModel> data;
 
     @Nullable
-    public static <T extends PlayerModel> T register(@NotNull T model) {
-        return data.add(model) ? model : null;
+    public static PlayerModel register(Identifier id, @NotNull PlayerModel model) {
+        return data.put(id, model);
+    }
+
+    @Nullable
+    public static PlayerModel get(Identifier id) {
+        return data.get(id);
     }
 
     @NotNull
-    public static Set<? extends PlayerModel> entries() {
-        return data;
+    public static Set<Map.Entry<Identifier, PlayerModel>> entries() {
+        return data.entrySet();
     }
 
     static {
-        data = new HashSet<>();
+        data = new HashMap<>();
     }
 }
--- src/client/java/com/codingcat/modelshifter/client/impl/model/Models.java
+++ src/client/java/com/codingcat/modelshifter/client/impl/model/Models.java
@@ -1,10 +1,16 @@
 package com.codingcat.modelshifter.client.impl.model;
 
+import com.codingcat.modelshifter.client.ModelShifterClient;
 import com.codingcat.modelshifter.client.api.model.PlayerModel;
 import com.codingcat.modelshifter.client.api.registry.ModelRegistry;
+import net.minecraft.util.Identifier;
 
 public class Models {
-    public static final PlayerModel CAT_PLAYER_MODEL = ModelRegistry.register(new CatPlayerModel());
-    public static final PlayerModel AMONG_US_PLAYER_MODEL = ModelRegistry.register(new AmongUsPlayerModel());
-    public static final PlayerModel WITHER_PLAYER_MODEL = ModelRegistry.register(new WitherPlayerModel());
+    public static final PlayerModel CAT_PLAYER_MODEL = register("cat", new CatPlayerModel());
+    public static final PlayerModel AMONG_US_PLAYER_MODEL = register("among_us", new AmongUsPlayerModel());
+    public static final PlayerModel WITHER_PLAYER_MODEL = register("wither", new AmongUsPlayerModel());
+
+    private static PlayerModel register(String id, PlayerModel playerModel) {
+        return ModelRegistry.register(new Identifier(ModelShifterClient.MOD_ID, id), playerModel);
+    }
 }
