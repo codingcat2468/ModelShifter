7383cb091b6c61bb74c5ef6166997b03e1a3d9ea
codingcat2468 <lw-app-produktion@gmx.net>
2024-09-09 03:32:25 +0200

    Change to 1.0.1

--- src/client/java/com/codingcat/modelshifter/client/gui/screen/ModelSelectionScreen.java
+++ src/client/java/com/codingcat/modelshifter/client/gui/screen/ModelSelectionScreen.java
@@ -9,6 +9,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.widget.OptionListWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
