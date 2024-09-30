95be7e6ebeba44a5d7053c7a2943f8f9340cad49
codingcat2468 <lw-app-produktion@gmx.net>
2024-10-01 01:32:54 +0200

    Fix ReloadModelRegistryCommand not updating the currently active model

--- src/client/java/com/codingcat/modelshifter/client/commands/dev/ReloadModelRegistryCommand.java
+++ src/client/java/com/codingcat/modelshifter/client/commands/dev/ReloadModelRegistryCommand.java
@@ -1,13 +1,16 @@
 package com.codingcat.modelshifter.client.commands.dev;
 
+import com.codingcat.modelshifter.client.ModelShifterClient;
 import com.codingcat.modelshifter.client.api.commands.ModCommand;
 import com.codingcat.modelshifter.client.api.registry.ModelRegistry;
+import com.codingcat.modelshifter.client.api.renderer.AdditionalRendererState;
 import com.codingcat.modelshifter.client.impl.Models;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public class ReloadModelRegistryCommand implements ModCommand<FabricClientCommandSource> {
     private static final Text RELOAD_SUCCESS = Text.translatable("modelshifter.commmand.dev.reloadmodelregistry");
@@ -18,11 +21,19 @@ public class ReloadModelRegistryCommand implements ModCommand<FabricClientComman
                 .executes(this::execute));
     }
 
+    @SuppressWarnings({"DataFlowIssue", "OptionalGetWithoutIsPresent"})
     public int execute(CommandContext<FabricClientCommandSource> context) {
-        ModelRegistry.clear();
-        Models.registerAll();
-        context.getSource().sendFeedback(RELOAD_SUCCESS);
+        try {
+            ModelRegistry.clear();
+            Models.registerAll();
+            AdditionalRendererState state = ModelShifterClient.state.getGlobalState();
+            Identifier id = ModelRegistry.findId(state.getPlayerModel()).orElse(null);
+            ModelShifterClient.state.setGlobalState(state.isRendererEnabled(), ModelRegistry.get(id).get());
+            context.getSource().sendFeedback(RELOAD_SUCCESS);
 
-        return 1;
+            return 1;
+        } catch (Exception e) {
+            return 0;
+        }
     }
 }
